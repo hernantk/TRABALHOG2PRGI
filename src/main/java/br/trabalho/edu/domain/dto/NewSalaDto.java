d98c5cd3451c777c16bde5b6a24dd958cdc607f0
hernan <Hernantkup@gmail.com>
2020-10-30 19:15:00 -0300

    DTOS

---
+++ src/main/java/br/trabalho/edu/domain/dto/NewSalaDto.java
@@ -0,0 +1,8 @@
+package br.trabalho.edu.domain.dto;
+
+public class NewSalaDto {
+
+    private String materia;
+    private String semestre;
+    private Integer userid;
+}
---
+++ src/main/java/br/trabalho/edu/domain/dto/SalaDto.java
@@ -0,0 +1,8 @@
+package br.trabalho.edu.domain.dto;
+
+public class SalaDto {
+
+    private Integer id;
+    private String materia;
+    private String semestre;
+}
