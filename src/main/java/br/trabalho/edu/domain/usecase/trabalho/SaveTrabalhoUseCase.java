4685be3096548928734f935ec136504b07820da0
hernan <Hernantkup@gmail.com>
2020-11-15 19:42:31 -0300

    Trabalho

---
+++ src/main/java/br/trabalho/edu/domain/usecase/trabalho/SaveTrabalhoUseCase.java
@@ -0,0 +1,34 @@
+package br.trabalho.edu.domain.usecase.trabalho;
+
+import br.trabalho.edu.data.entity.Trabalho;
+import br.trabalho.edu.data.entity.User;
+import br.trabalho.edu.data.repository.TrabalhoRepository;
+import br.trabalho.edu.domain.dto.trabalho.NewTrabalhoDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class SaveTrabalhoUseCase {
+
+    private TrabalhoRepository repository;
+
+    public void execute(NewTrabalhoDto dto){
+
+        Trabalho trabalho = new Trabalho();
+
+        trabalho.setDataentrega(dto.getDataEntrega());
+        trabalho.setDatapostagem(dto.getDataPostagem());
+        trabalho.setDescricao(dto.getDescricao());
+        trabalho.setNota(dto.getNota());
+        trabalho.setTitulo(dto.getTitulo());
+
+        User user = new User();
+        user.setUser_id(dto.getIduser());
+        trabalho.setUser(user);
+
+
+
+        repository.save(trabalho);
+    }
+}
