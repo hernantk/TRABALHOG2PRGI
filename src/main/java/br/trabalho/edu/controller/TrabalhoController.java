287b712d3ad4811b3530a7b4972723958703b3d6
hernan <Hernantkup@gmail.com>
2020-11-17 12:33:18 -0300

    Trabalho

--- src/main/java/br/trabalho/edu/controller/TrabalhoController.java
+++ src/main/java/br/trabalho/edu/controller/TrabalhoController.java
@@ -6,13 +6,16 @@ import br.trabalho.edu.domain.dto.trabalho.TrabalhoDto;
 
 import br.trabalho.edu.domain.usecase.trabalho.FindAllTrabalhoUseCase;
 import br.trabalho.edu.domain.usecase.trabalho.SaveTrabalhoUseCase;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/trabalho")
 public class TrabalhoController {
 
 
